cf8c159b4d2e46d7b3b4cdc9730dde72c97c95e4
angeloshmangelo <angeloshmangelo@gmail.com>
2021-11-03 16:54:30 -0700

    Initial Commit

---
+++ test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by Angelo Mercado on 11/3/21.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
